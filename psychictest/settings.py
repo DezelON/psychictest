276c46b3bfaecd906b7c12eac1ce96257d3af393
DezelON <popenkodaniil@mail.ru>
2020-04-20 08:45:35 -0700

    Add Settings. Add gitignore.

--- psychictest/settings.py
+++ psychictest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pe4j9$qqsxn4t(a&_@i(s8cl95u-zzc&=#xo&p0y&_1n3zkypk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "193.164.16.48"]
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'psychictest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,9 +75,11 @@ WSGI_APPLICATION = 'psychictest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'psychic',
+        'USER': 'testuser',
+        'PASSWORD': '12345678',
+    },
 }
 
 
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
